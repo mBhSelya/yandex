86dcd4e202b49e1f5017bfbcd185c4c81a38e477
mBhSelya <selyakoff18@gmail.com>
2020-12-13 18:04:59 +0300

    123123

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
-let m = 10,
-  n = 10,
-  arr = [],
+let arr = [],
+  // m = document.getElementById('mValue').value,
+  // n = document.getElementById('nValue').value,
+  // x = document.getElementById('xValue').value,
+  m,
+  n,
+  x = 1,
   arr2 = [],
   sosedi = 0
-for (i = 0; i < m * n; i++) {
-  arr2.push(Math.round(Math.random() * 1))
+
+const creat = function (m, n) {
+  m = document.getElementById('mValue').value
+  n = document.getElementById('nValue').value
+  for (i = 0; i < m * n; i++) {
+    arr2.push(Math.floor(Math.random() * 2))
+  }
+  return arr2
 }
-//console.log(arr2)
-console.log('------------------------------')
-const func = function (arr2, sosedi, m, arr, n) {
+
+
+const func = function (arr2, sosedi, arr, m, n) {
+  n = document.getElementById('nValue').value
+  m = document.getElementById('mValue').value
+  console.log('------------------------------')
+  document.getElementById('inBorder').innerHTML = ""
   arr = []
   for (let i = 0; i <= m * n - 1; i++) {
     if (arr2[i - 1]) { sosedi++ }
@@ -34,12 +48,19 @@ const func = function (arr2, sosedi, m, arr, n) {
     sosedi = 0
     arr.push(arr2[i])
     if (i !== 0 && (i + 1) % m == 0) {
+      for (let i in arr) {
+        document.getElementById("inBorder").innerHTML += '<span>' + '| ' + arr[i] + ' ' + '</span>'
+      }
+      document.getElementById("inBorder").innerHTML += '|' + '<br>'
       console.log(arr)
       arr = []
     }
   }
-  console.log('------------------------------')
 }
 
-let timer = setInterval(() => func(arr2, sosedi, m, arr, n), 1000)
-setTimeout(() => { clearInterval(timer); console.log('stop'); }, 2000)
+
+const timerFunc = function () {
+  let timer = setInterval(() => func(arr2, sosedi, m, arr, n), 1000)
+  setTimeout(() => { clearInterval(timer); console.log('------------------------------'); }, (x * 1000))
+
+}

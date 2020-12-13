0cb2a76aaaaef94f5b76e1c05308489d2eb0d075
mBhSelya <selyakoff18@gmail.com>
2020-12-13 21:17:50 +0300

    123

--- app.js
+++ app.js
@@ -1,36 +1,30 @@
 let arr = [],
-  // m = document.getElementById('mValue').value,
-  // n = document.getElementById('nValue').value,
-  // x = document.getElementById('xValue').value,
-  m = 10,
-  n = 10,
   x = 1,
   arr2 = [],
   sosedi = 0
 
-const creat = function (m, n) {
-  m = document.getElementById('mValue').value
-  n = document.getElementById('nValue').value
+const creat = function () {
+  m = Number(document.getElementById('mValue').value)
+  n = Number(document.getElementById('nValue').value)
   for (i = 0; i < m * n; i++) {
     arr2.push(Math.floor(Math.random() * 2))
   }
 }
 
 
-const func = function (arr2, sosedi, m, arr, n) {
-
+const func = function () {
   console.log('------------------------------')
   document.getElementById('inBorder').innerHTML = ""
   arr = []
   for (let i = 0; i <= m * n - 1; i++) {
-    if (arr2[i - 1]) { sosedi++ }
-    if (arr2[i - 1]) { sosedi++ }
-    if (arr2[i - m - 1]) { sosedi++ }
-    if (arr2[i - m + 1]) { sosedi++ }
-    if (arr2[i - m]) { sosedi++ }
-    if (arr2[i + m]) { sosedi++ }
-    if (arr2[i + m - 1]) { sosedi++ }
-    if (arr2[i + m + 1]) { sosedi++ }
+    if (arr2[i - 1]) { ++sosedi }
+    if (arr2[i - 1]) { ++sosedi }
+    if (arr2[i - m - 1]) { ++sosedi }
+    if (arr2[i - m + 1]) { ++sosedi }
+    if (arr2[i - m]) { ++sosedi }
+    if (arr2[i + m]) { ++sosedi }
+    if (arr2[i + m - 1]) { ++sosedi }
+    if (arr2[i + m + 1]) { ++sosedi }
 
     if (arr2[i] === 1) {
       if (sosedi === 2 || sosedi === 3) {
@@ -57,6 +51,7 @@ const func = function (arr2, sosedi, m, arr, n) {
 
 }
 const timerFunc = function () {
+  x = Number(document.getElementById('xValue').value)
   let timer = setInterval(() => func(arr2, sosedi, m, arr, n), 1000)
   setTimeout(() => { clearInterval(timer); console.log('------------------------------'); }, (x * 1000))
 

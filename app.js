79e15854c96ad900a985e5b17609c21420c50ef1
mBhSelya <selyakoff18@gmail.com>
2020-12-14 04:01:16 +0300

    123

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 let arr = [],
   x = 1,
   arr2 = [],
+  arr3 = [],
   sosedi = 0
 
 const creat = function () {
+  document.getElementById('inBorder').innerHTML = ""
   m = Number(document.getElementById('mValue').value)
   n = Number(document.getElementById('nValue').value)
   for (i = 0; i < m * n; i++) {
-    arr2.push(Math.floor(Math.random() * 2))
+    arr.push(Math.floor(Math.random() * 2))
+    if (i !== 0 && (i + 1) % m == 0) {
+      Array.prototype.push.apply(arr2, arr)
+      for (let i in arr) {
+        document.getElementById("inBorder").innerHTML += '<span>' + '| ' + arr[i] + ' ' + '</span>'
+      }
+      document.getElementById("inBorder").innerHTML += '|' + '<br>'
+      arr = []
+    }
   }
+  arr = []
 }
 
 
@@ -50,9 +61,42 @@ const func = function () {
   }
 
 }
+
+
 const timerFunc = function () {
   x = Number(document.getElementById('xValue').value)
   let timer = setInterval(() => func(arr2, sosedi, m, arr, n), 1000)
   setTimeout(() => { clearInterval(timer); console.log('------------------------------'); }, (x * 1000))
+}
 
+const disDisabled = function () {
+  document.getElementById('startButt').removeAttribute('disabled')
+}
+
+function readFile(input) {
+  let file = input.files[0]
+  let reader = new FileReader()
+  reader.readAsText(file)
+  reader.onload = function () {
+    console.log(reader.result)
+    arr2 = reader.result.split('')
+    arr2.map((x, i) => { arr2[i] = Number(x) })
+    console.log(arr2)
+    for (i = 0; i < arr2.length; i++) {
+      if (i !== 0 && (i + 1) % m == 0) {
+        Array.prototype.push.apply(arr, arr2)
+        for (let i in arr) {
+          document.getElementById("inBorder").innerHTML += '<span>' + '| ' + arr[i] + ' ' + '</span>'
+        }
+        document.getElementById("inBorder").innerHTML += '|' + '<br>'
+        arr = []
+      }
+    }
+    arr = []
+    console.log(arr2.length)
+  }
+
+  reader.onerror = function () {
+    console.log(reader.error)
+  }
 }
